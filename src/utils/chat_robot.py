d5ac7e9966abf68e49448a9f0028613526aceadf
chubin.zheng <chubin.zheng@qizhidao.com>
2024-06-02 12:31:04 +0800

    聊天机器人

--- src/utils/chat_robot.py
+++ src/utils/chat_robot.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*--
 # Chat GPT 工具类
 import os
-from typing import Union
+from typing import Union, List
 from openai import OpenAI
 
 
@@ -15,26 +15,44 @@ class OpenAiChat(object):
     # base_url = "https://apiok.us/api/openai/v1"
 
     @classmethod
-    def get_completion(cls, prompt: str, model: str = "gpt-3.5-turbo",
-                       timeout: int = 60, temperature: int = 0) -> Union[str]:
+    def get_completion_from_messages(cls, messages: List[dict], model: str = "gpt-3.5-turbo",
+                                     temperature: int = 0, timeout: int = 60):
         """
-        封装 OpenAI 接口的函数，参数为 Prompt，返回对应结果
         文档：https://platform.openai.com/docs/quickstart
-        prompt: 对应的提示词
-        model: 调用的模型，默认为 gpt-3.5-turbo(ChatGPT)，有内测资格的用户可以选择 gpt-4
-        temperature: 模型输出的温度系数，控制输出的随机程度
+        :param messages:
+            1、系统消息(system message)：以系统身份发送消息，提供了一个总体的指示。有助于设置助手的行为和角色，并作为对话的高级指示。
+            2、用户消息(user message)：在 ChatGPT 网页界面中，用户发送的消息称为用户消息
+            3、助手消息(assistant message)：ChatGPT 的消息称为助手消息。
+            示例:
+                messages =  [
+                    {'role':'system', 'content':'You are an assistant that speaks like Shakespeare.'},
+                    {'role':'user', 'content':'tell me a joke'},
+                    {'role':'assistant', 'content':'Why did the chicken cross the road'},
+                    {'role':'user', 'content':'I don\'t know'}
+                ]
+        :param model: 调用的模型，默认为 gpt-3.5-turbo(ChatGPT)，有内测资格的用户可以选择 gpt-4
+        :param temperature: 模型输出的温度系数，控制输出的随机程度
+        :param timeout: 超时时间
         """
         client = OpenAI(api_key=cls.api_key, base_url=cls.base_url)
         completion = client.chat.completions.create(
-            messages=[{"role": "user", "content": prompt}],
+            messages=messages,
             model=model,
             timeout=timeout,
             temperature=temperature,
         )
-        # 调用 OpenAI 的 ChatCompletion 接口
         if len(completion.choices) > 0:
             return completion.choices[0].message.content
         return None
+
+    @classmethod
+    def get_completion(cls, prompt: str, model: str = "gpt-3.5-turbo",
+                       timeout: int = 60, temperature: int = 0) -> Union[str]:
+        """
+        封装 OpenAI 接口的函数，参数为 Prompt，返回对应结果
+        """
+        message = [{"role": "user", "content": prompt}]
+        return cls.get_completion_from_messages(message, model=model, temperature=temperature, timeout=timeout)
 
 
 if __name__ == "__main__":
