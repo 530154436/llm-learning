08a20e4e49587e2148191e3d5f94c35b052e6e10
chubin.zheng <chubin.zheng@qizhidao.com>
2024-06-03 21:20:05 +0800

    add token count

--- src/utils/chat_robot.py
+++ src/utils/chat_robot.py
@@ -2,70 +2,105 @@
 # -*- coding:utf-8 -*--
 # Chat GPT 工具类
 import os
-from typing import Union, List
+from typing import List, Tuple
 from openai import OpenAI
+from openai.types import CompletionUsage
+from openai.types.chat import ChatCompletion
 
 
 class OpenAiChat(object):
     # api_key = os.environ.get("OPENAI_API_KEY")
     # base_url = "https://api.openai.com/v1"
-    api_key = os.environ.get("CHAT_ANYWHERE_API_KEY")
-    base_url = "https://api.chatanywhere.com.cn/v1"
     # api_key = os.environ.get("IDATARIVER_API_KEY")
     # base_url = "https://apiok.us/api/openai/v1"
+    api_key = os.environ.get("CHAT_ANYWHERE_API_KEY")
+    base_url = "https://api.chatanywhere.com.cn/v1"
 
-    @classmethod
-    def get_completion_from_messages(cls, messages: List[dict], model: str = "gpt-3.5-turbo",
-                                     temperature: int = 0, timeout: int = 60):
+    def __init__(self, model: str = "gpt-3.5-turbo", temperature: int = 0,
+                 max_tokens: int = 500, timeout: int = 60):
         """
         文档：https://platform.openai.com/docs/quickstart
-        :param messages:
-            1、系统消息(system message)：以系统身份发送消息，提供了一个总体的指示。有助于设置助手的行为和角色，并作为对话的高级指示。
-            2、用户消息(user message)：在 ChatGPT 网页界面中，用户发送的消息称为用户消息
-            3、助手消息(assistant message)：ChatGPT 的消息称为助手消息。
-            示例:
-                messages =  [
-                    {'role':'system', 'content':'You are an assistant that speaks like Shakespeare.'},
-                    {'role':'user', 'content':'tell me a joke'},
-                    {'role':'assistant', 'content':'Why did the chicken cross the road'},
-                    {'role':'user', 'content':'I don\'t know'}
-                ]
         :param model: 调用的模型，默认为 gpt-3.5-turbo(ChatGPT)，有内测资格的用户可以选择 gpt-4
-        :param temperature: 模型输出的温度系数，控制输出的随机程度
+        :param temperature: 模型输出的温度系数，控制输出的随机程度；值越大，生成的回复越随机。默认为 0。
+        :param max_tokens: 生成回复的最大 token 数量。默认为 500。
         :param timeout: 超时时间
         """
-        client = OpenAI(api_key=cls.api_key, base_url=cls.base_url)
-        completion = client.chat.completions.create(
+        self.model: str = model
+        self.temperature: int = temperature
+        self.max_tokens: int = max_tokens
+        self.timeout: int = timeout
+        self.client = OpenAI(api_key=self.api_key, base_url=self.base_url)
+
+    def create_completion(self, messages: List[dict]) -> ChatCompletion:
+        """
+        1、系统消息(system message)：以系统身份发送消息，提供了一个总体的指示。有助于设置助手的行为和角色，并作为对话的高级指示。
+        2、用户消息(user message)：在 ChatGPT 网页界面中，用户发送的消息称为用户消息
+        3、助手消息(assistant message)：ChatGPT 的消息称为助手消息。
+        :example
+            messages =  [
+                {'role':'system', 'content':'You are an assistant that speaks like Shakespeare.'},
+                {'role':'user', 'content':'tell me a joke'},
+                {'role':'assistant', 'content':'Why did the chicken cross the road'},
+                {'role':'user', 'content':'I don\'t know'}
+            ]
+        """
+        completion = self.client.chat.completions.create(
             messages=messages,
-            model=model,
-            timeout=timeout,
-            temperature=temperature,
+            model=self.model,
+            timeout=self.timeout,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens
         )
+        return completion
+
+    def get_completion_from_messages(self, messages: List[dict]) -> str:
+        """
+        返回生成的回复内容
+        """
+        completion = self.create_completion(messages)
         if len(completion.choices) > 0:
             return completion.choices[0].message.content
         return None
 
-    @classmethod
-    def get_completion(cls, prompt: str, model: str = "gpt-3.5-turbo",
-                       timeout: int = 60, temperature: int = 0) -> Union[str]:
+    def get_completion(self, prompt: str) -> str:
         """
-        封装 OpenAI 接口的函数，参数为 Prompt，返回对应结果
+        返回生成的回复内容
         """
         message = [{"role": "user", "content": prompt}]
-        return cls.get_completion_from_messages(message, model=model, temperature=temperature, timeout=timeout)
+        return self.get_completion_from_messages(message)
+
+    def get_completion_and_token_count(self, messages: List[dict]) -> Tuple[str, dict]:
+        """
+        返回生成的回复内容以及使用的 token 数量。
+        :return
+            content: 生成的回复内容。
+            token_dict: 包含'prompt_tokens'、'completion_tokens'和'total_tokens'的字典，
+                        分别表示提示的 token 数量、生成的回复的 token 数量和总的 token 数量。
+        """
+        completion = self.create_completion(messages)
+        if len(completion.choices) > 0:
+            content = completion.choices[0].message.content
+            usage: CompletionUsage = completion.usage
+            token_dict = {
+                'prompt_tokens': usage.prompt_tokens,
+                'completion_tokens': usage.completion_tokens,
+                'total_tokens': usage.total_tokens,
+            }
+            return content, token_dict
+        return None
 
 
 if __name__ == "__main__":
-    _text = f"""
-    您应该提供尽可能清晰、具体的指示，以表达您希望模型执行的任务。\
-    这将引导模型朝向所需的输出，并降低收到无关或不正确响应的可能性。\
-    不要将写清晰的提示词与写简短的提示词混淆。\
-    在许多情况下，更长的提示词可以为模型提供更多的清晰度和上下文信息，从而导致更详细和相关的输出。
-    """
+    _text = "中国的首都是哪里"
     _prompt = f"""
     请将三个反引号之间的文本总结成一句话。
     ```{_text}```
     """
-    # 指令内容，使用 ``` 来分隔指令和待总结的内容
-    response = OpenAiChat.get_completion(_prompt)
-    print(response)
+    chat_robot = OpenAiChat()
+    # print(chat_robot.get_completion(_prompt))
+
+    _messages = [
+        {'role': 'system', 'content': '你是一个助理， 并以 Seuss 苏斯博士的风格作出回答。'},
+        {'role': 'user', 'content': '就快乐的小鲸鱼为主题给我写一首短诗'}
+    ]
+    print(chat_robot.get_completion_and_token_count(_messages))
