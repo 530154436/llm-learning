049a362fe5e4d43552a3dad863b28de64734066b
chubin.zheng <chubin.zheng@qizhidao.com>
2024-11-11 15:14:48 +0800

    添加Dckerfile

---
+++ llm_app_deploy/app_vllm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#conda activate base
+
+nohup python -m vllm.entrypoints.openai.api_server \
+--host 0.0.0.0 \
+--port 8000 \
+--model data/models/Qwen2.5-7B-Instruct \
+--served-model-name Qwen2.5-7B-Instruct \
+--gpu-memory-utilization 0.8 \
+--max-model-len 2560 > server.log &
