c63eae9109d807f3690b9a72a3dd4d98bd0793ed
chubin.zheng <chubin.zheng@qizhidao.com>
2024-11-12 20:46:11 +0800

    add fastapi demo

--- llm_app_deploy/app_vllm.sh
+++ llm_app_deploy/app_vllm.sh
@@ -7,3 +7,16 @@ nohup python -m vllm.entrypoints.openai.api_server \
 --served-model-name Qwen2.5-7B-Instruct \
 --gpu-memory-utilization 0.9 \
 --max-model-len 2400 > server.log &
+
+# 测试服务
+#curl http://localhost:8000/v1/chat/completions -H "Content-Type: application/json" -d '{
+#  "model": "Qwen2.5-7B-Instruct",
+#  "messages": [
+#    {"role": "system", "content": "你是Qwen，由阿里云创建。你是一个乐于助人的助手。"},
+#    {"role": "user", "content": "你是谁？"}
+#  ],
+#  "temperature": 0.7,
+#  "top_p": 0.8,
+#  "repetition_penalty": 1.05,
+#  "max_tokens": 512
+#}'
