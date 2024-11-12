fdb6ed431acb064dafb3e9bf252cf67469167be7
chubin.zheng <chubin.zheng@qizhidao.com>
2024-11-12 20:36:05 +0800

    add fastapi demo

--- llm_app_deploy/app_vllm.sh
+++ llm_app_deploy/app_vllm.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#conda activate base
-
+cd $PYTHON_PATH # ProjectHome
 nohup python -m vllm.entrypoints.openai.api_server \
 --host 0.0.0.0 \
 --port 8000 \
 --model data/models/Qwen2.5-7B-Instruct \
 --served-model-name Qwen2.5-7B-Instruct \
---gpu-memory-utilization 0.8 \
+--gpu-memory-utilization 0.9 \
 --max-model-len 2400 > server.log &
