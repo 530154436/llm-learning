cbc7877d1ce879a47dece16ee9ba156e5be35cff
chubin.zheng <chubin.zheng@qizhidao.com>
2024-11-12 20:11:53 +0800

    添加安装包

--- llm_app_deploy/app_fastapi.py
+++ llm_app_deploy/app_fastapi.py
@@ -13,6 +13,11 @@ DEVICE = "cuda"  # 使用CUDA
 DEVICE_ID = "0"  # CUDA设备ID，如果未设置则为空
 CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE  # 组合CUDA设备信息
 
+# 加载预训练的分词器和模型
+model_name_or_path = 'dev-nfs/zhengchubin/llm-learning/data/models/Qwen2.5-7B-Instruct'
+TOKENIZER = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=False)
+MODEL = AutoModelForCausalLM.from_pretrained(model_name_or_path, device_map="auto", torch_dtype=torch.bfloat16)
+
 
 def clear_torch_cache():
     gc.collect()
@@ -36,25 +41,23 @@ app = FastAPI()
 # 处理POST请求的端点
 @app.post("/")
 async def create_item(request: Request):
-    global model, tokenizer  # 声明全局变量以便在函数内部使用模型和分词器
     json_post_raw = await request.json()  # 获取POST请求的JSON数据
     json_post = json.dumps(json_post_raw)  # 将JSON数据转换为字符串
     json_post_list = json.loads(json_post)  # 将字符串转换为Python对象
     prompt = json_post_list.get('prompt')  # 获取请求中的提示
-
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": prompt}
     ]
 
     # 调用模型进行对话生成
-    input_ids = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-    model_inputs = tokenizer([input_ids], return_tensors="pt").to('cuda')
-    generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=512)
+    input_ids = TOKENIZER.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    model_inputs = TOKENIZER([input_ids], return_tensors="pt").to('cuda')
+    generated_ids = MODEL.generate(model_inputs.input_ids, max_new_tokens=512)
     generated_ids = [
         output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
     ]
-    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    response = TOKENIZER.batch_decode(generated_ids, skip_special_tokens=True)[0]
     now = datetime.datetime.now()  # 获取当前时间
     time = now.strftime("%Y-%m-%d %H:%M:%S")  # 格式化时间为字符串
     # 构建响应JSON
@@ -72,11 +75,6 @@ async def create_item(request: Request):
 
 # 主函数入口
 if __name__ == '__main__':
-    # 加载预训练的分词器和模型
-    model_name_or_path = '/root/autodl-tmp/qwen/Qwen2.5-7B-Instruct'
-    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=False)
-    model = AutoModelForCausalLM.from_pretrained(model_name_or_path, device_map="auto", torch_dtype=torch.bfloat16)
-
     # 启动FastAPI应用
     # 用6006端口可以将autodl的端口映射到本地，从而在本地使用api
     uvicorn.run(app, host='0.0.0.0', port=6006, workers=1)  # 在指定端口和主机上启动应用
