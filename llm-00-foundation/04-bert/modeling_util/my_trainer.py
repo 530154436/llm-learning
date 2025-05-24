fa8c1e245f696b4b3ba51f5b47b1c138fe9aaf8a
chubin.zheng <chubin.zheng@qizhidao.com>
2025-05-24 13:13:23 +0800

    1

--- llm-00-foundation/04-bert/modeling_util/my_trainer.py
+++ llm-00-foundation/04-bert/modeling_util/my_trainer.py
@@ -121,7 +121,7 @@ class MyTrainer(object):
                 y_pred = self.model.predict(*xy_tuple[:-1]).to(self.device).contiguous().view(-1)
                 y_true = y_true.contiguous().view(-1)
                 assert y_pred.shape == y_true.shape
-                print(y_true.device, y_pred.device, ", ".join(str(v.device) for v in self.metrics.values()))
+                # print(y_true.device, y_pred.device, ", ".join(str(v.device) for v in self.metrics.values()))
                 self.metrics.update(y_pred, y_true)
 
         result = {"val_loss": round(total_loss / step, 5)}
